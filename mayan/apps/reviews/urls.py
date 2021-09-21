15b343629e46a57d1373781b7f2e9c660ca0d64e
tyjlowe <tylerlowe@gmail.com>
2021-09-21 00:10:26 -0400

    Register 'reviews' django app & Add sample urls

--- mayan/apps/reviews/urls.py
+++ mayan/apps/reviews/urls.py
@@ -1,1 +1,12 @@
 ### Application Routing File ###
+from django.conf.urls import url
+from .. import cabinets
+
+urlpatterns = [
+    url(
+        # sample URL for the URL pattern, OBVIOUSLY you would link to a more
+        # appropriate view!
+        regex=r'^reviews/$', name='cabinet_list',
+        view=cabinets.views.CabinetListView.as_view()
+        )
+]
